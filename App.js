b3cc2da316c2930ea9e9325e7c1ef76b15becb58
Gauravbhadauria <gauravofficial1995@gmail.com>
2024-04-02 10:49:24 +0530

    done

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import {View, Text, Alert, SafeAreaView} from 'react-native';
 import React, {useState} from 'react';
-import CustomTextinput from './src/components/CustomTextinput';
-import CustomButton from './src/components/CustomButton';
-import CustomBottamNav from './src/components/CustomBottamNav';
-import Screen1 from './Screen1';
-import Screen2 from './Screen2';
-import Screen3 from './Screen3';
-import Screen4 from './Screen4';
-import CustomWelcome from './src/components/CustomWelcome';
-import { Header } from './src/Index';
-
+import {Header,Button} from '@engineercodewala/components'
 
 
 const App = () => {
@@ -17,36 +8,7 @@ const App = () => {
   const [selectedTab, setSelectedTab] = useState(0);
   return (
     <SafeAreaView style={{flex: 1}}>
-      {/* <CustomWelcome
-
-        screens={[
-          {
-            image: require('./slide1.jpg'),
-            title: 'Slide 1',
-            desc: 'slide 1 description',
-          },
-          {
-            image: require('./slide2.png'),
-            title: 'Slide 1',
-            desc: 'slide 1 description',
-          },
-          {
-            image: require('./slide1.jpg'),
-            title: 'Slide 3',
-            desc: 'slide 3 description',
-          },
-          {
-            image: require('./slide2.png'),
-            title: 'Slide 4',
-            desc: 'slide 4 description',
-          },
-        ]}
-        onContinue={() => {
-          //navigation
-        }}
-        activeDotColor={'red'}
-        // nonActiveDotColor={'#f2f2f2'}
-      /> */}
+      <Button title='Btn by ec'/>
       <Header
         title={'Home'}
         rightIconStyle={{width:30,height:30}}
@@ -60,6 +22,7 @@ const App = () => {
         leftIcon={require('./menu.png')}
         rightIcons={[require('./loupe.png'), require('./bell.png')]}
       />
+      
     </SafeAreaView>
   );
 };
